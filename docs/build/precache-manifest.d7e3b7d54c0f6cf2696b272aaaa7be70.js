c1199f56d0c515ac6b0598b13fc51514c1498f8b
Alexander Makeev <master.gennadij@gmail.com>
2020-04-18 15:23:18 +0300

    latest build

---
+++ docs/build/precache-manifest.d7e3b7d54c0f6cf2696b272aaaa7be70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82382c1117fb01aebcb8e3b88d7306fe",
+    "url": "/react-hooks-tetris/index.html"
+  },
+  {
+    "revision": "6c94cc38d786e9e663cf",
+    "url": "/react-hooks-tetris/static/css/main.03d25919.chunk.css"
+  },
+  {
+    "revision": "0e5407761a48bba81cce",
+    "url": "/react-hooks-tetris/static/js/2.58a58aea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hooks-tetris/static/js/2.58a58aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c94cc38d786e9e663cf",
+    "url": "/react-hooks-tetris/static/js/main.588dbeac.chunk.js"
+  },
+  {
+    "revision": "35ae17fe5c8e98160e7a",
+    "url": "/react-hooks-tetris/static/js/runtime-main.e7387260.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-hooks-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-hooks-tetris/static/media/bg.2810fe6b.png"
+  }
+]);
